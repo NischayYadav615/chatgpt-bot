7346ac7df16cf5de9c94a6fa0c2f939c351d27f0
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2025-01-10 12:56:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = environ.get("API_ID" , "2099007")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
+API_ID = environ.get("API_ID" , "13963336")
+API_HASH = environ.get("API_HASH" , "a144d1e22ef0b29738e8c00713d02678")
 BOT_TOKEN = environ.get("BOT_TOKEN" , "2099007:AAH1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
 ADMIN = int(environ.get("ADMIN" , "1847899007"))
 CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
-MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002440952521")
+MONGO_URL = environ.get("MONGO_URL" , "mongodb+srv://Nischay999:Nischay999@cluster0.5kufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 AUTH_CHANNEL = int(
     environ.get("AUTH_CHANNEL", "-1001734958816")
 )
